75eab80949117d9578cba1171101b4528325d684
Raymond Loman <raymond@carpago.nl>
2019-03-20 11:09:28 +0100

    Add assignment for Parsing Double

--- src/nl/joost/animals/substrings/Application.java
+++ src/nl/joost/animals/substrings/Application.java
@@ -4,6 +4,11 @@ package nl.joost.animals.substrings;
 public class Application {
 
    public static void main(String[] args) {
+    
+      oefeningMaken();
+   }
+   
+   public static void oefeningMaken() {
       String s = "--0.50";
       String substring = s.substring(1, s.length()-1);
       
@@ -16,6 +21,11 @@ public class Application {
          System.out.println("Dat klopt niet, maak de oefening zelf af!!!!");
          
       }
+      
+      // Parse het result naar een Double
+      double r = Double.parseDouble("");// maak de oefening zelf af.
+      
+      System.out.println(r);
    }
    
    
